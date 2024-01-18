67314839f7bd18b160ad39b22b24e5c8dc0e9e17
Akintola Babatunde Rafiu <tundegabriel999@gmail.com>
2024-01-18 02:47:18 +0100

    first-commit

---
+++ src/ToggleSounds.js
@@ -0,0 +1,16 @@
+import { memo } from "react";
+
+function ToggleSounds({ allowSound, setAllowSound }) {
+  return (
+    <button
+      className="btn-sound"
+      onClick={() => setAllowSound((allow) => !allow)}
+    >
+      {allowSound ? "🔈" : "🔇"}
+    </button>
+  );
+}
+
+//here since we aree exporting it , we can just memoize down here like this
+
+export default memo(ToggleSounds);
